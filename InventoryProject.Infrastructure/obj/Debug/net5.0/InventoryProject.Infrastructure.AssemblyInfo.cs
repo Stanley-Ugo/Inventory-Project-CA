83530796cc37c77a4167f769de039ebb84952c19
Rankshow <uwaomaobinna20@gmail.com>
2024-03-25 16:22:56 +0100

    Add await to the Repository layer lacking await

--- InventoryProject.Infrastructure/obj/Debug/net5.0/InventoryProject.Infrastructure.AssemblyInfo.cs
+++ InventoryProject.Infrastructure/obj/Debug/net5.0/InventoryProject.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryProject.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f777b26bd5a0ddfb2e576cd2a8a3495fdb4e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryProject.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryProject.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
